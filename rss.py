890a591f282811077073a47cf674a82ef733fc67
Dknow01 <87356298+Dknow01@users.noreply.github.com>
2021-08-24 19:18:14 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -52,7 +52,7 @@ def create_feed_checker(feed_url):
             elif "Porn" in entry.link:
                 message = f"Hidden Naughty Stuff"
             else:
-                message = f"/mirror {entry.link} \n\nTitle ⏩ {entry.title}"
+                message = f"/mirror {entry.link} \n\nTitle ⏩ {entry.title} \nSize⏩ {entry.size}"
             try:
                 msg = app.send_message(log_channel, message)
                 db.update_link(feed_url, entry.id)
