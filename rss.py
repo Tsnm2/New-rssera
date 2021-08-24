471ea9a41f71e75f12118049de8c048359feb4c2
Dknow01 <87356298+Dknow01@users.noreply.github.com>
2021-08-24 06:32:54 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -51,7 +51,7 @@ def create_feed_checker(feed_url):
             except Exception as e:
                 print(e)
         else:
-            print( f"Checked RSS FEED: /mirror {entry.id}")
+            print( f"Checked RSS FEED: /mirror {entry.link}| {entry.title}")
     return check_feed
 
 
